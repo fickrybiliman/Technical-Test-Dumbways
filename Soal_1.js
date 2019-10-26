b0c1ff03a71cfa08dd8a5348a3e3259676394d9e
fickrybiliman <bil.iman.fickry@gmail.com>
2019-10-26 09:57:35 +0700

    soal no 1, fix done

--- Soal_1.js
+++ Soal_1.js
@@ -1,11 +1,17 @@
 let dataKey = ['dumb','ways','the','best'];
 let word = 'dumbways';
+let result = ''
 
-function matching(data, word) {
+function check(data, word) {
    for (let i = 0; i < data.length; i++) {
-      console.log(`${data[i]} => ${word.includes(data[i])}`);
+      if (i === data.length - 1) {
+         result += `${data[i]} => ${word.includes(data[i])}`;
+      } else {
+         result += `${data[i]} => ${word.includes(data[i])}\n`;
+      }
    }
    // console.log(`${data.map(x => x + ' => ' + word.includes(x) + '\n')}`)
+   return result;
 }
 
-console.log(matching(dataKey, word));
+console.log(check(dataKey, word));
