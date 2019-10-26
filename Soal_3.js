855d756456b5463f85d6af7e0f209c7d80207c7e
fickrybiliman <bil.iman.fickry@gmail.com>
2019-10-26 10:26:38 +0700

    soal no 3, done

---
+++ Soal_3.js
@@ -0,0 +1,26 @@
+let result = '';
+
+function cetak_gambar(bilangan) {
+   for (let i = 0; i < bilangan; i++) {
+      for (let j = 0; j < bilangan; j++) {
+         if (i % 2 !== 0) {
+            if (j !== 0 && j !== bilangan - 1) {
+               result += '=  ';
+            } else if (j === bilangan - 1) {
+               result += '*\n';
+            } else {
+               result += '*  ';
+            }
+         } else {
+            if (j === bilangan - 1) {
+               result += '*\n';
+            } else {
+               result += '*  ';
+            }
+         }      
+      }
+   }
+   return result;
+};
+
+console.log(cetak_gambar(5));
